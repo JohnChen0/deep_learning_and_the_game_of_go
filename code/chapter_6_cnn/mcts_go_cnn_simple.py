2e0db53363141a6dd964630637e1aa1e1be362fe
John Chen <john_chen@outlook.com>
2020-04-13 18:56:10 -0700

    6.3 & 6.4
    copy some files from master branch.

--- code/chapter_6_cnn/mcts_go_cnn_simple.py
+++ code/chapter_6_cnn/mcts_go_cnn_simple.py
@@ -7,8 +7,8 @@ from keras.layers import Dense
 from keras.layers import Conv2D, Flatten  # <1>
 
 np.random.seed(123)
-X = np.load('../generated_games/features-200.npy')
-Y = np.load('../generated_games/labels-200.npy')
+X = np.load('../generated_games/features-40k.npy')
+Y = np.load('../generated_games/labels-40k.npy')
 
 samples = X.shape[0]
 size = 9
@@ -16,7 +16,7 @@ input_shape = (size, size, 1)  # <2>
 
 X = X.reshape(samples, size, size, 1)  # <3>
 
-train_samples = 10000
+train_samples = int(0.9 * samples)
 X_train, X_test = X[:train_samples], X[train_samples:]
 Y_train, Y_test = Y[:train_samples], Y[train_samples:]
 
@@ -27,24 +27,28 @@ Y_train, Y_test = Y[:train_samples], Y[train_samples:]
 
 # tag::mcts_go_cnn_simple_model[]
 model = Sequential()
-model.add(Conv2D(filters=32,  # <1>
+model.add(Conv2D(filters=48,  # <1>
                  kernel_size=(3, 3),  # <2>
                  activation='sigmoid',
+                 padding='same',      # <3>
                  input_shape=input_shape))
 
-model.add(Conv2D(64, (3, 3), activation='sigmoid'))  # <3>
+model.add(Conv2D(48, (3, 3),          # <4>
+                 padding='same',
+                 activation='sigmoid'))
 
-model.add(Flatten())  # <4>
+model.add(Flatten())  # <5>
 
-model.add(Dense(128, activation='sigmoid'))
-model.add(Dense(size * size, activation='sigmoid'))  # <5>
+model.add(Dense(512, activation='sigmoid'))
+model.add(Dense(size * size, activation='sigmoid'))  # <6>
 model.summary()
 
-# <1> The first layer in our network is a Conv2D layer with 32 output filters.
+# <1> The first layer in our network is a Conv2D layer with 48 output filters.
 # <2> For this layer we choose a 3 by 3 convolutional kernel.
-# <3> The second layer in another convolution. We leave out the "filters" and "kernel_size" arguments for brevity.
-# <4> We then flatten the 3D output of the previous convolutional layer...
-# <5> ... and follow up with two more dense layers, as we did in the MLP example.
+# <3> Normally the output of a convolution is smaller than the input. By adding `padding='same'`, we ask Keras to pad our matrix with zeros around the edges, so the output has the same dimension as the input.
+# <4> The second layer is another convolution. We leave out the "filters" and "kernel_size" arguments for brevity.
+# <5> We then flatten the 3D output of the previous convolutional layer...
+# <6> ... and follow up with two more dense layers, as we did in the MLP example.
 # end::mcts_go_cnn_simple_model[]
 
 # tag::mcts_go_cnn_simple_eval[]
@@ -54,7 +58,7 @@ model.compile(loss='mean_squared_error',
 
 model.fit(X_train, Y_train,
           batch_size=64,
-          epochs=5,
+          epochs=15,
           verbose=1,
           validation_data=(X_test, Y_test))
 
--- code/chapter_6_cnn/mcts_go_mlp.py
+++ code/chapter_6_cnn/mcts_go_mlp.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+# TODO: tell readers where to put feature and label files in text
 
 # tag::mcts_go_mlp_preprocessing[]
 import numpy as np
@@ -6,25 +7,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 np.random.seed(123)  # <1>
-# TODO: tell readers where to put file
-X = np.load('../generated_games/features-200.npy')  # <2>
-Y = np.load('../generated_games/labels-200.npy')
+X = np.load('../generated_games/features-40k.npy')  # <2>
+Y = np.load('../generated_games/labels-40k.npy')
 samples = X.shape[0]
 board_size = 9 * 9
 
 X = X.reshape(samples, board_size)  # <3>
 Y = Y.reshape(samples, board_size)
 
-train_samples = 10000
+train_samples = int(0.9 * samples)  # <4>
 X_train, X_test = X[:train_samples], X[train_samples:]
 Y_train, Y_test = Y[:train_samples], Y[train_samples:]
 # end::mcts_go_mlp_preprocessing[]
 
 # tag::mcts_go_mlp_model[]
 model = Sequential()
-model.add(Dense(200, activation='sigmoid', input_shape=(board_size,)))
-model.add(Dense(300, activation='sigmoid'))
-model.add(Dense(200, activation='sigmoid'))
+model.add(Dense(1000, activation='sigmoid', input_shape=(board_size,)))
+model.add(Dense(500, activation='sigmoid'))
 model.add(Dense(board_size, activation='sigmoid'))
 model.summary()
 
@@ -34,7 +33,7 @@ model.compile(loss='mean_squared_error',
 
 model.fit(X_train, Y_train,
           batch_size=64,
-          epochs=5,
+          epochs=15,
           verbose=1,
           validation_data=(X_test, Y_test))
 
@@ -42,3 +41,26 @@ score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 # end::mcts_go_mlp_model[]
+
+
+# tag::mcts_go_evaluate[]
+test_board = np.array([[
+    0, 0,  0,  0,  0, 0, 0, 0, 0,
+    0, 0,  0,  0,  0, 0, 0, 0, 0,
+    0, 0,  0,  0,  0, 0, 0, 0, 0,
+    0, 1, -1,  1, -1, 0, 0, 0, 0,
+    0, 1, -1,  1, -1, 0, 0, 0, 0,
+    0, 0,  1, -1,  0, 0, 0, 0, 0,
+    0, 0,  0,  0,  0, 0, 0, 0, 0,
+    0, 0,  0,  0,  0, 0, 0, 0, 0,
+    0, 0,  0,  0,  0, 0, 0, 0, 0,
+]])
+move_probs = model.predict(test_board)[0]
+i = 0
+for row in range(9):
+    row_formatted = []
+    for col in range(9):
+        row_formatted.append('{:.3f}'.format(move_probs[i]))
+        i += 1
+    print(' '.join(row_formatted))
+# end::mcts_go_evaluate[]
